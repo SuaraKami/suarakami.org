a8ed458bea2c332bc628480e1d4b1747c1e071a9
frenico <16384641+frenicohansen@users.noreply.github.com>
2025-10-10 17:24:27 +0200

    Remove production analytics script from layout
    Removed production analytics script from layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,6 @@
 	import '../app.css';
 
 	let { children } = $props();
-	const isProd = import.meta.env.PROD;
 </script>
-
-<svelte:head>
-	{#if isProd}
-		<script
-			defer
-			data-domain="bio.suarakami.id"
-			src="https://analytics.suarakami.id/js/script.js"
-		></script>
-	{/if}
-</svelte:head>
 
 {@render children()}
